a355f1f11c9ed1500868e3723a04b156ecda93b9
Alvaro Cavalcanti <alvarovictor@gmail.com>
2016-03-17 23:01:20 -0300

    Adicionados pacotes necessarios para a aplicacao DRF

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,10 @@
 apt-get update
 
 # install python
-apt-get install python-dev python-pip -q -y
+apt-get install libpq-dev python-dev python-pip -q -y
+
+# install extra packages
+apt-get install postgresql python-psycopg2
 
 # install project dependencies
 pip install -r /vagrant/codigo/requirements.txt
